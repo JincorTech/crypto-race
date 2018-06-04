fc95c77757e53828eeb23928d5d2d181e2ba3190
Alexander <aleksand-isaev@yandex.ru>
2018-06-05 00:03:14 +0400

    Added classnames

--- src/app/containers/App/index.js
+++ src/app/containers/App/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import cx from 'classnames';
+
 import s from './styles.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className={s.caption}>Hello, world!</div>
+      <div className={cx(s.caption, s.container)}>Hello, world!</div>
     )
   }
 }
