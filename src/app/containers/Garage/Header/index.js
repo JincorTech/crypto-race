c0fc255272ee980fe7825afda5615875761bd3c5
Alexander <aleksand-isaev@yandex.ru>
2018-06-06 00:02:20 +0400

    Placed menu

--- src/app/containers/Garage/Header/index.js
+++ src/app/containers/Garage/Header/index.js
@@ -5,7 +5,12 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className={s.container}>
-        Header dummy
+        <div className={s.linksBlock}>
+          <span className={s.link}>RACES</span>|
+          <span className={s.link}>GARAGE</span>|
+          <span className={s.link}>SHOP</span>|
+          <span className={s.link}>LOGOUT</span>
+        </div>
       </div>
     )
   }
