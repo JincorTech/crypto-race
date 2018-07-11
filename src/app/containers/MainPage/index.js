8d02ae76aff15a59a9b173aa66ba7668909ca0e8
Alexander <aleksand-isaev@yandex.ru>
2018-07-11 14:23:50 +0400

    Added ships

--- src/app/containers/MainPage/index.js
+++ src/app/containers/MainPage/index.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import s from './styles.css';
 
+const Ship1 = '/assets/images/main_page/ship-1.png';
+const Ship2 = '/assets/images/main_page/ship-2.png';
+const Ship3 = '/assets/images/main_page/ship-3.png';
+
 export default class MainPage extends React.Component {
   render() {
     return (
@@ -10,6 +14,17 @@ export default class MainPage extends React.Component {
         <div className={s.startSection}>
         </div>
         <div className={s.bottomSection}>
+          <div className={s.shipsContainer}>
+            <div className={s.ship}>
+              <img className={s.shipImg} src={Ship1} />
+            </div>
+            <div className={s.ship}>
+              <img className={s.shipImg} src={Ship2} />
+            </div>
+            <div className={s.ship}>
+              <img className={s.shipImg} src={Ship3} />
+            </div>
+          </div>
         </div>
       </div>
     )
