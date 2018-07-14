3b1328a8ef5368aef6a6bb5082a4e314f3dc8364
Alexander <aleksand-isaev@yandex.ru>
2018-07-14 18:11:21 +0400

    Added footer

--- src/app/containers/App/Footer/index.js
+++ src/app/containers/App/Footer/index.js
@@ -5,13 +5,17 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className={s.container}>
-        {/* <div className={s.textBlock}>
-          <div className={s.logo}>TO THE MOON ©</div>
+        <div className={s.textBlock}>
+          <div className={s.logo}>TO THE MOON<span className={s.crSign}>©</span></div>
           <div className={s.links}>
-            <a href='#'>Contacts</a> | <a href='#'>Terms of Use</a> | <a href='#'>Privacy Policy</a>
+            <a href='#'>Contacts</a>
+            <span className={s.divider}>|</span>
+            <a href='#'>Terms of Use</a>
+            <span className={s.divider}>|</span>
+            <a href='#'>Privacy Policy</a>
           </div>
           <div className={s.copyright}>© 2018 All rights Reserved</div>
-        </div> */}
+        </div>
       </div>
     )
   }
