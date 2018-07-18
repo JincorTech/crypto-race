7ea0666a3e027ffe7871aff774c0a902812b2806
Alexander <aleksand-isaev@yandex.ru>
2018-07-19 00:17:15 +0400

    Fixed slider

--- src/app/containers/Garage/ShipSettings/ShipCarousel/index.js
+++ src/app/containers/Garage/ShipSettings/ShipCarousel/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import cx from 'classnames';
+import Slider from 'react-slick';
 import s from './styles.css';
 
 const PlaceImg = '/assets/images/your_ship/place.png';
@@ -7,19 +8,48 @@ const ShipImg = '/assets/images/your_ship/ship.png';
 const ArrowLeftImg = '/assets/images/your_ship/arrow_left.png';
 const ArrowRightImg = '/assets/images/your_ship/arrow_right.png';
 
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <img className={cx(s.arrow, s.arrowRight)} src={ArrowRightImg} onClick={onClick}/>
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <img className={cx(s.arrow, s.arrowLeft)} src={ArrowLeftImg} onClick={onClick}/>
+  );
+}
+
 export default class ShipCarousel extends React.Component {
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />
+    };
+
+    const ships = [
+      ShipImg,
+      ShipImg,
+      ShipImg,
+    ]
+    
     return (
       <div className={s.container}>
         <img className={s.place} src={PlaceImg} />
-        <img className={s.ship} src={ShipImg} />
-        <img className={s.arrowLeft} src={ArrowLeftImg} />
-        <img className={s.arrowRight} src={ArrowRightImg} />
-        <div className={s.pages}>
-          <div className={cx(s.pageItem, s.active)} />
-          <div className={s.pageItem} />
-          <div className={s.pageItem} />
-        </div>
+        <Slider {...settings}>
+          {ships.map((ship, index) => (
+            <div key={index} className={s.item}>
+              <img className={s.ship} src={ship} />
+            </div>
+          ))}
+        </Slider>
       </div>
     )
   }
