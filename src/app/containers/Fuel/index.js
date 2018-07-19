28f1a1e0ac52e3d4206d3fe9c31f55219f3b8073
Alexander <aleksand-isaev@yandex.ru>
2018-07-19 11:14:59 +0400

    Fixed Header and Footer

--- src/app/containers/Fuel/index.js
+++ src/app/containers/Fuel/index.js
@@ -4,6 +4,8 @@ import Caption from './Caption';
 import Indicator from 'components/Indicator';
 import Button from 'components/Button';
 import Avatar from 'components/Avatar';
+import Header from 'components/main/Header';
+import Footer from 'components/main/Footer';
 import routes from 'routes';
 
 const FuelImg = '/assets/images/fuel/fuel.png';
@@ -52,38 +54,42 @@ export default class Fuel extends React.Component {
     } = this.state;
 
     return (
-      <div className={s.container}>
-        <Caption icon={FuelImg} text='CHOOSE FUEL' />
-        <div className={s.avatarContainer}>
-          <Avatar />
-          <div className={s.avatarText}>
-            Fill your ship and go to the moon!
+      <React.Fragment>
+        <Header />
+        <div className={s.container}>
+          <Caption icon={FuelImg} text='CHOOSE FUEL' />
+          <div className={s.avatarContainer}>
+            <Avatar />
+            <div className={s.avatarText}>
+              Fill your ship and go to the moon!
           </div>
-        </div>
-        <div className={s.body}>
-          <div className={s.indicators}>
-            {Indicators.map((item, index) => (
-              <div key={item.name} className={s.indicator}>
-                <Indicator isInput onChange={(value) => this.handleChangeIndicator(value, index)} thumbClass={item.thumbClass} name={item.name} level={levels[index]} length={20} color={item.color} />
+          </div>
+          <div className={s.body}>
+            <div className={s.indicators}>
+              {Indicators.map((item, index) => (
+                <div key={item.name} className={s.indicator}>
+                  <Indicator isInput onChange={(value) => this.handleChangeIndicator(value, index)} thumbClass={item.thumbClass} name={item.name} level={levels[index]} length={20} color={item.color} />
+                </div>
+              ))}
+              <div className={s.totalIndicator}>
+                <Indicator name={'TOTAL'} level={100 - this.getTotal(levels)} length={20} color={'#fff'} />
               </div>
-            ))}
-            <div className={s.totalIndicator}>
-              <Indicator name={'TOTAL'} level={100 - this.getTotal(levels)} length={20} color={'#fff'} />
-            </div>
-            {/* <div className={s.text}>
+              {/* <div className={s.text}>
               Fill your ship
             </div> */}
-            <div className={s.buttons}>
-              {/* <div className={s.addButton}>
+              <div className={s.buttons}>
+                {/* <div className={s.addButton}>
                 <Button text="+ADD" color="#3593eb" />
               </div> */}
-              <Button to={routes.game} text="2THEMOON" color="#ed1c24" />
+                <Button to={routes.game} text="2THEMOON" color="#ed1c24" />
+              </div>
             </div>
+            <img className={s.ship} src={ShipImg} />
+            <img className={s.station} src={StationImg} />
           </div>
-          <img className={s.ship} src={ShipImg} />
-          <img className={s.station} src={StationImg} />
         </div>
-      </div>
+        <Footer />
+      </React.Fragment>
     )
   }
 }
--- src/app/containers/Garage/index.js
+++ src/app/containers/Garage/index.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import s from './styles.css';
 
+import Header from 'components/main/Header';
+import Footer from 'components/main/Footer';
 import Layout from './Layout';
+
 
 export default class Garage extends React.Component {
   render() {
     return (
-      <Layout />
+      <React.Fragment>
+        <Header />
+        <Layout />
+        <Footer />
+      </React.Fragment>
     )
   }
 }
