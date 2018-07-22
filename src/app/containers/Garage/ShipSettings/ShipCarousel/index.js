9e0ff9a170660e673fa0c1b5acfb1392bb596223
Alexander <aleksand-isaev@yandex.ru>
2018-07-22 15:25:25 +0400

    Added ship

--- src/app/containers/Garage/ShipSettings/ShipCarousel/index.js
+++ src/app/containers/Garage/ShipSettings/ShipCarousel/index.js
@@ -13,6 +13,7 @@ const Ship3Img = '/assets/images/your_ship/ship-3.gif';
 const Ship4Img = '/assets/images/your_ship/ship-4.gif';
 const Ship5Img = '/assets/images/your_ship/ship-5.gif';
 const Ship6Img = '/assets/images/your_ship/ship-6.gif';
+const Ship7Img = '/assets/images/your_ship/ship-7.gif';
 
 // temporary ship size hack
 function getClassNameByIndex(index) {
@@ -45,6 +46,7 @@ const ShipCarousel = () => {
   };
 
   const ships = [
+    Ship7Img,
     Ship5Img,
     Ship6Img,
     Ship1Img,
