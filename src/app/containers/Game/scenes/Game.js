c4f4083f7bdda720745761577ebaee9bc1d6e950
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-07-22 13:49:09 +0500

    Extreme programming results

--- src/app/containers/Game/scenes/Game.js
+++ src/app/containers/Game/scenes/Game.js
@@ -1,11 +1,9 @@
 import Phaser from 'phaser';
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 import isEqual from 'deep-equal';
 
-import fns from '../utils/players';
-import { getId } from '../../../utils/auth';
-
-// const socket = io.connect('http://localhost:4000/game');
+import players from '../utils/players';
+import { getToken } from '../../../utils/auth';
 
 const leftStartFrame = 11;
 const leftEndFrame = 18;
@@ -21,9 +19,10 @@ export default class Game extends Phaser.Scene {
     super({ key: 'game' });
 
     this.state = {
-      id: getId(),
+      id: '',
       left: false,
-      right: false
+      right: false,
+      x: 23
     };
 
     this.commonContext = {};
@@ -53,11 +52,11 @@ export default class Game extends Phaser.Scene {
     this.load.image('space3', '/assets/game/background/space3-2.png');
     this.load.image('hole', '/assets/game/background/hole.png');
 
-    this.load.spritesheet('ship:nova', '/assets/game/ships/nova.png', { frameWidth: 130, frameHeight: 132 });
-    this.load.spritesheet('ship:omega', '/assets/game/ships/omega.png', { frameWidth: 130, frameHeight: 132 });
+    this.load.spritesheet('nova', '/assets/game/ships/nova.png', { frameWidth: 130, frameHeight: 132 });
+    this.load.spritesheet('omega', '/assets/game/ships/omega.png', { frameWidth: 130, frameHeight: 132 });
   }
 
-  create(players) {
+  create(data) {
     this.enemies = this.physics.add.group();
     this.planets = this.add.group();
 
@@ -71,80 +70,95 @@ export default class Game extends Phaser.Scene {
     this.moon = this.add.tileSprite((this.screenWidth / 2) - 180, -180, 360, 360, 'planet:moon').setOrigin(0);
 
     const percHeight = (window.innerHeight - 256 - 130) / 100;
-    fns.spawnPlayers(this, players, percHeight, window.innerWidth);
+    players.spawnPlayers(this, data.player, data.enemies, percHeight);
 
     //  Input Events
     this.commonContext.cursors = this.input.keyboard.createCursorKeys();
 
-    // socket.on('strafeSync', (data) => {
-    //   const enemy = this.enemies.children.get('id', data.id);
-    //
-    //   if (this.player.id === data.id) {
-    //     if (data.left) {
-    //       this.player.setVelocityX(-1 * PlayerSpeed);
-    //     } else if (data.right) {
-    //       this.player.setVelocityX(PlayerSpeed);
-    //     } else {
-    //       this.player.setVelocityX(0);
-    //     }
-    //   } else {
-    //     if (data.left) {
-    //       this.enemies.children.get('id', data.id).setVelocityX(-1 * PlayerSpeed);
-    //     } else if (data.right) {
-    //       this.enemies.children.get('id', data.id).setVelocityX(PlayerSpeed);
-    //     } else {
-    //       this.enemies.children.get('id', data.id).setVelocityX(0);
-    //     }
-    //   }
+    this.state.id = this.player.id;
+
+    // this.socket = io('https://game-api.secrettech.io/race', { query: `token=${getToken()}` });
+    // console.log(window.globalSocket);
+    window.globalSocket.on('error', (e) => console.log(e));
+    // window.globalSocket.on('init', (data) => console.log(data));
+    // window.globalSocket.on('joined', (data) => console.log(data));
+    // window.globalSocket.emit('moveX', this.state);
+    // window.globalSocket.on('moveXupdate', (data) => {
+    //   console.log('on', data);
     // });
+
+    window.globalSocket.on('moveXupdate', (data) => {
+      console.log('on', data);
+      // const enemy = this.enemies.children.get('id', data.id);
+
+      if (this.player.id === data.id) {
+        if (data.left) {
+          this.player.setVelocityX(-1 * PlayerSpeed);
+        } else if (data.right) {
+          this.player.setVelocityX(PlayerSpeed);
+        } else {
+          this.player.setVelocityX(0);
+        }
+      } else {
+        if (data.left) {
+          this.enemies.children.get('id', data.id).setVelocityX(-1 * PlayerSpeed);
+        } else if (data.right) {
+          this.enemies.children.get('id', data.id).setVelocityX(PlayerSpeed);
+        } else {
+          this.enemies.children.get('id', data.id).setVelocityX(0);
+        }
+      }
+    });
   }
 
   update() {
     const newState = {
-      id: getId(),
+      id: this.player.id,
       left: false,
-      right: false
+      right: false,
+      x: 23
     };
 
     if (this.commonContext.cursors.left.isDown) {
       newState.left = true;
       newState.right = false;
 
-      if (!this.player.anims.currentFrame || this.player.anims.currentAnim.key !== 'player_left' || this.player.anims.currentFrame.index < 8) {
-        this.player.anims.play('player_left', true);
-      } else {
-        this.player.anims.stop('player_left');
-      }
+      // if (!this.player.anims.currentFrame || this.player.anims.currentAnim.key !== 'player_left' || this.player.anims.currentFrame.index < 8) {
+      //   this.player.anims.play('player_left', true);
+      // } else {
+      //   this.player.anims.stop('player_left');
+      // }
     } else if (this.commonContext.cursors.right.isDown) {
       newState.left = false;
       newState.right = true;
 
-      if (!this.player.anims.currentFrame || this.player.anims.currentAnim.key !== 'player_right' || this.player.anims.currentFrame.index < 10) {
-        this.player.anims.play('player_right', true);
-      } else {
-        this.player.anims.stop('player_right');
-      }
+      // if (!this.player.anims.currentFrame || this.player.anims.currentAnim.key !== 'player_right' || this.player.anims.currentFrame.index < 10) {
+      //   this.player.anims.play('player_right', true);
+      // } else {
+      //   this.player.anims.stop('player_right');
+      // }
     } else {
       newState.left = false;
       newState.right = false;
 
-      if (leftStartFrame < this.player.frame.name
-        && this.player.frame.name <= leftEndFrame) {
-        this.player.anims.play('player_left_back', true);
-      } else if (rightStartFrame < this.player.frame.name
-        && this.player.frame.name <= rightEndFrame) {
-        this.player.anims.play('player_right_back', true);
-      } else {
-        this.player.anims.stop('player_left_back');
-        this.player.anims.stop('player_right_back');
-        this.player.setFrame(0);
-      }
+      // if (leftStartFrame < this.player.frame.name
+      //   && this.player.frame.name <= leftEndFrame) {
+      //   this.player.anims.play('player_left_back', true);
+      // } else if (rightStartFrame < this.player.frame.name
+      //   && this.player.frame.name <= rightEndFrame) {
+      //   this.player.anims.play('player_right_back', true);
+      // } else {
+      //   this.player.anims.stop('player_left_back');
+      //   this.player.anims.stop('player_right_back');
+      //   this.player.setFrame(0);
+      // }
     }
 
     if (!isEqual(this.state, newState)) {
       this.state.left = newState.left;
       this.state.right = newState.right;
-      // socket.emit('strafe', this.state);
+      window.globalSocket.emit('moveX', this.state);
+      console.log('emit', this.state);
     }
 
     const getRandY = () => (Math.random() * (0.5 - 1.5)) + 1;
--- src/app/containers/Game/utils/players.js
+++ src/app/containers/Game/utils/players.js
@@ -1,62 +1,26 @@
 import ships from './ships';
 
-// count Y axis from user % progress
+const spawnPlayers = (self, player, enemies, percentHight) => {
+  const getY = (position) => (position === 0 ? 30 * percentHight : 60 * percentHight);
 
-// const spawnSinglePlayer = (self, player, totalPlayers, percentHight, screenWidth) => {
-//   console.log(player, self);
-//   const x = screenWidth / (totalPlayers + 1);
-//   const y = (player.progress * percentHight) + 256 + 65;
-// };
-
-const spawnPlayers = (self, players, percentHight, screenWidth) => {
-  const spawn = (player, index, array) => {
-    const totalPlayers = array.length;
-    const x = (screenWidth / (totalPlayers + 1)) * (index + 1);
-    const y = (player.progress * percentHight) + 256 + 65;
-
-    if (player.owner) {
-      self.player = self.physics.add.sprite(x, y, player.ship.type);
-      self.player.setCollideWorldBounds(true);
-      self.player.id = player.id;
-      ships.createShipAnimation(self, player);
-    } else {
-      const enemy = self.physics.add.sprite(x, y, player.ship.type);
-      enemy.id = player.id;
-      self.enemies.add(enemy);
-      ships.createShipAnimation(self, player);
-    }
+  const spawnPlayer = (player) => {
+    self.player = self.physics.add.sprite(player.x, getY(player.position), player.ship.type);
+    self.player.setCollideWorldBounds(true);
+    self.player.id = player.id;
+    ships.createShipAnimation(self, player);
   };
 
-  players.forEach(spawn);
+  const spawnEnemy = (enemy) => {
+    const obj = self.physics.add.sprite(enemy.x, getY(enemy.position), enemy.ship.type);
+    obj.id = enemy.id;
+    self.enemies.add(obj);
+    ships.createShipAnimation(self, enemy);
+  };
+
+  spawnPlayer(player);
+  spawnEnemy(enemies);
 };
 
-function addPlayer(self, player, percHeight, width) {
-  const positionY = (player.progress * percHeight) + 256 + 65;
-  const positionX = width / 3;
-  self.player =
-    self
-      .physics
-      .add
-      .sprite(positionX, positionY, player.ship.type);
-  self.player.setCollideWorldBounds(true);
-  ships.createShipAnimation(self, player);
-}
-
-function addEnemy(self, player, percHeight, width) {
-  const positionY = (player.progress * percHeight) + 256 + 65;
-  const positionX = (width / 3) * 2;
-  const _enemy =
-    self
-      .physics
-      .add
-      .sprite(positionX, positionY, player.ship.type);
-  _enemy.id = player.id;
-  self.enemies.add(_enemy);
-  ships.createShipAnimation(self, player);
-}
-
 export default {
-  addPlayer,
-  addEnemy,
   spawnPlayers
 };
