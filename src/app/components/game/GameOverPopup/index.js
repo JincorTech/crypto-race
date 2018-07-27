51c04502695c260c3fffdba44e03959591098371
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-07-27 19:28:54 +0500

    game over screen

---
+++ src/app/components/game/GameOverPopup/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import s from './styles.css';
+
+const GameOverPopup = (props) => {
+  const renderPlayer = (player) => (
+    <div className={s.player}>
+      #{player.position} {player.name} | {player.score} pts | {player.prize} ETH
+    </div>
+  );
+
+  return (
+    <div className={s.popup}>
+      <div className={s.title}>GAME OVER</div>
+
+      <div className={s.players}>
+        {props.players.map(renderPlayer)}
+      </div>
+
+      <div className={s.linkWrap}>
+        <Link className={s.link} to="/garage">
+          BACK TO GARAGE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default GameOverPopup;
--- src/app/containers/game/Game/index.js
+++ src/app/containers/game/Game/index.js
@@ -3,20 +3,28 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Phaser from 'phaser';
 import queryString from 'query-string';
-// import io from 'socket.io-client';
 import isArrayEqual from 'array-equal';
 
 import { fetchInitialData } from '../../../redux/modules/game/game';
 
 import Game from './scenes/Game';
+import GameOverPopup from '../../../components/game/GameOverPopup';
 import Topbar from '../../../components/game/Topbar';
 import Map from '../../../components/game/Map';
 import Chat from '../../../components/game/Chat';
 import Profile from '../../../components/game/Profile';
 import s from './styles.css';
-// import { getToken, getEmail } from '../../../utils/auth';
 
 class GameContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gameover: false,
+      players: []
+    }
+  }
+
   componentDidMount() {
     window.game = new Phaser.Game({
       type: Phaser.AUTO,
@@ -38,11 +46,9 @@ class GameContainer extends React.Component {
     const { trackId } = queryString.parse(this.props.location.search);
     const { players } = this.props;
 
-    console.log('game container mounted ------------------------------------------------');
     console.log('game: ', window.game);
     console.log('players: ', players);
     console.log('track id: ', trackId);
-
 
     if (!this.props.players.length > 0) {
       window.socket.emit('loadTrack', { trackId });
@@ -50,36 +56,10 @@ class GameContainer extends React.Component {
       window.game.scene.start('game', { trackId, players: this.props.players });
     }
 
-
-    // window.globalSocket = io.connect('https://game-api.secrettech.io/race', { query: `token=${getToken()}` });
-
-    // window.globalSocket.on('connect', () => {
-    //   window.globalSocket.on('init', (data) => {
-    //     const { players, ...rest } = data;
-    //     const player = players.filter((p) => p.email === getEmail())[0];
-    //     const enemies = players.filter((p) => p.email !== getEmail())[0];
-    //     this.props.fetchInitialData({ player, enemies, ...rest });
-
-    //     window.game = new Phaser.Game({
-    //       type: Phaser.AUTO,
-    //       width: window.innerWidth,
-    //       height: window.innerHeight,
-    //       physics: {
-    //         default: 'arcade',
-    //         arcade: {
-    //           gravity: { y: 0 },
-    //           debug: false
-    //         }
-    //       },
-    //       parent: 'content',
-    //       scene: [
-    //         Game
-    //       ]
-    //     });
-
-    //     window.game.scene.start('game', { player, enemies });
-    //   });
-    // });
+    window.socket.on('gameover', (players) => {
+      this.setState({ gameover: true, players });
+      window.game.scene.pause('game');
+    });
   }
 
   componentDidUpdate(prevProps) {
@@ -113,6 +93,7 @@ class GameContainer extends React.Component {
         <div className={s.profile}><Profile player={player}/></div>
         <div className={s.backdrop}/>
         <div className={s.container} id="content"></div>
+        {this.state.gameover && <div className={s.gameover}><GameOverPopup players={this.state.players}/></div>}
       </div>
     );
   }
