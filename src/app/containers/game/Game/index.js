c2c4aa0ad24f856b9fd056318528fe9351a3361d
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-07-30 21:52:12 +0500

    breaking fix

--- src/app/containers/game/Game/index.js
+++ src/app/containers/game/Game/index.js
@@ -101,13 +101,13 @@ class GameContainer extends React.Component {
     console.log('players: ', players);
     console.log('track id: ', trackId);
 
-    window.game.scene.start('game', { trackId, players: PLAYERS_MOCK });
+    // window.game.scene.start('game', { trackId, players: PLAYERS_MOCK });
 
-    // if (!this.props.players.length > 0) {
-    //   window.socket.emit('loadTrack', { trackId });
-    // } else {
-    //   window.game.scene.start('game', { trackId, players: this.props.players });
-    // }
+    if (!this.props.players.length > 0) {
+      window.socket.emit('loadTrack', { trackId });
+    } else {
+      window.game.scene.start('game', { trackId, players: this.props.players });
+    }
 
     window.socket.on('gameover', (players) => {
       this.setState({ gameover: true, players });
@@ -115,17 +115,17 @@ class GameContainer extends React.Component {
     });
   }
 
-  // componentDidUpdate(prevProps) {
-  //   if (!isArrayEqual(this.props.players, prevProps.players)) {
-  //     console.log('new props arrived');
-  //     if (!window.game.scene.isProcessing) {
-  //       console.log('if game not started - start it with new props');
-  //       console.log(this.props.players);
-  //       const { trackId } = queryString.parse(this.props.location.search);
-  //       window.game.scene.start('game', { trackId, players: this.props.players });
-  //     }
-  //   }
-  // }
+  componentDidUpdate(prevProps) {
+    if (!isArrayEqual(this.props.players, prevProps.players)) {
+      console.log('new props arrived');
+      if (!window.game.scene.isProcessing) {
+        console.log('if game not started - start it with new props');
+        console.log(this.props.players);
+        const { trackId } = queryString.parse(this.props.location.search);
+        window.game.scene.start('game', { trackId, players: this.props.players });
+      }
+    }
+  }
 
   componentWillUnmount() {
     if (window.game) window.game.destroy();
