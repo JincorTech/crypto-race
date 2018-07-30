a6bc015023faf2bcc428d79de9cdad86e7dd67e5
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-07-30 17:58:58 +0500

    Fetch profile in garage

--- src/app/components/common/Avatar/index.js
+++ src/app/components/common/Avatar/index.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import s from './styles.css';
 
-export default class Avatar extends React.Component {
-  render() {
-    return (
-      <div className={s.avatar}>
-        <img className={s.avatarImage} src='/assets/images/user_info/avatar.png' />
-      </div>
-    )
-  }
-}
+const Avatar = ({ url }) => (
+  <div className={s.avatar}>
+    <img className={s.avatarImage} src={url || '/assets/images/user_info/avatar.png'}/>
+  </div>
+);
+
+export default Avatar;
