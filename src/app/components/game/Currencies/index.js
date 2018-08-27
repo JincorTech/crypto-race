f4325e488757d35d4da6f21886274977c9fdf079
Alexander <aleksand-isaev@yandex.ru>
2018-08-27 19:32:02 +0400

    Fix currencies style

--- src/app/components/game/Currencies/index.js
+++ src/app/components/game/Currencies/index.js
@@ -18,7 +18,7 @@ class Currencies extends Component {
         {Object.getOwnPropertyNames(currencies).sort().map((ticker) => {
           const value = ((currenciesStart[ticker] - currencies[ticker]) / currencies[ticker] * 100).toFixed(2);
           return (
-          <div key={ticker}>
+          <div className={s.item} key={ticker}>
             <span className={s.label}>{ticker}: </span>
             <span style={{color: this.chooseColor(value)}}>{value}%</span>
           </div>
