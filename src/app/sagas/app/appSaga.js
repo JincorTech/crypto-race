2332b95df4f6709adeafe9f17b63b1d7a5f311fe
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-09-04 16:14:11 +0500

    reconnect if socket desconnected after login

--- src/app/sagas/app/appSaga.js
+++ src/app/sagas/app/appSaga.js
@@ -1,5 +1,7 @@
 import { all, takeLatest, call, fork, put } from 'redux-saga/effects';
-import { push } from 'connected-react-router'
+import { push } from 'connected-react-router';
+import io from 'socket.io-client';
+
 import { setToken } from '../../utils/auth';
 import { get } from '../../utils/fetch';
 
@@ -11,7 +13,14 @@ function* signInIterator({ payload: token }) {
     const data = yield call(get, `/user/auth/facebook/token?access_token=${token}`);
     yield put(signIn.success());
     yield call(setToken, data.accessToken);
-    yield put(push('/garage'))
+
+    yield call(console.log, window.socket);
+
+    if (window.socket.disconnected) {
+      window.socket = io.connect('https://game-api.secrettech.io/', { query: `token=${data.accessToken}` });
+    }
+
+    yield put(push('/garage'));
   } catch (e) {
     console.log(e);
     yield put(signIn.failure());
